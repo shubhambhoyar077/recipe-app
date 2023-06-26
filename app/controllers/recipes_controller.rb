124a4871da63ca6f363491f69426da13d1a26d4a
shubham <shubham.bhoyar077@gmail.com>
2023-06-26 20:38:32 +0530

    Add create method for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,11 +11,21 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
-  # def create
-
-  # end
+  def create
+    @recipe = current_user.recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe), notice: 'Recipe is successfully Created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   # def destroy
 
   # end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :public, :description)
+  end
 end
