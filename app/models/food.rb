0a8fb5c13978f52d05670064d7cb74f825f1dc40
shubham <shubham.bhoyar077@gmail.com>
2023-06-26 15:12:23 +0530

    Add relation in model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,5 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipe_food
+  has_many :recipe, through: :recipe_food
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_food
+  has_many :food, through: :recipe_food
 end
