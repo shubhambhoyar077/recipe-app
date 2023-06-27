01471d693663a9cabb7a015a53e8018a72a8c734
shubham <shubham.bhoyar077@gmail.com>
2023-06-27 13:39:24 +0530

    Create controller for recipe_foods

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "recipes#index"
   resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  resources :recipe_foods, only: [:new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
