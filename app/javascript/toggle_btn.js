e35186e8297bb4708c952f5f1dfb7a6d64f907ba
shubham <shubham.bhoyar077@gmail.com>
2023-06-27 20:02:42 +0530

    Fix toggle btn on off issue

--- app/javascript/toggle_btn.js
+++ app/javascript/toggle_btn.js
@@ -1,5 +1,6 @@
 function initToggleBtn() {
   let toggleBtn = document.getElementById("toggle-btn");
+  toggleBtn.checked = toggleBtn.dataset.public === "true";
   let form = document.getElementById("recipe-form");
 
   toggleBtn.addEventListener("change", function () {
