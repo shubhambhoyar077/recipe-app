1edb7ceb5660c78b4dc3e5e66d823c61087939a2
edubew <edubew98@gmail.com>
2023-06-28 18:14:45 +0300

    Add public_recipe roures

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "recipes#index"
+  root "public_recipes#index"
   resources :recipes, only: [:index, :show, :new, :create, :update, :destroy] do
     resources :recipe_foods, only: [:new, :create, :destroy]
   end
   get 'generate_shopping_list', to: 'shopping_lists#generate'
+  get '/public_recipes', to: 'recipes#public', as: 'public_recipes'
   
   resources :foods
 end
