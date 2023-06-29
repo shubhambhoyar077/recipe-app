061b86185b9d6b4396c81244a4a66286948c733a
edubew <edubew98@gmail.com>
2023-06-29 18:21:09 +0300

    Fix rubocop errors

--- spec/integration/recipe_public_spec.rb
+++ spec/integration/recipe_public_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe 'User index page', type: :system do
   describe 'Recipe' do
     before do
       @user = User.create!(name: 'user1', email: 'user@email.com', password: '123456')
-      @recipe = Recipe.create!(user_id: @user.id,  name: 'New public recipe' , preparation_time: 20, cooking_time: 40, description: 'Recipe description', public: true)
-      @recipe = Recipe.create!(user_id: @user.id,  name: 'New public recipe' , preparation_time: 20, cooking_time: 40, description: 'Recipe description', public: false) 
+      @recipe = Recipe.create!(user_id: @user.id, name: 'New public recipe', preparation_time: 20, cooking_time: 40,
+                               description: 'Recipe description', public: true)
+      @recipe = Recipe.create!(user_id: @user.id, name: 'New public recipe', preparation_time: 20, cooking_time: 40,
+                               description: 'Recipe description', public: false)
       visit public_recipes_path
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: @user.password
